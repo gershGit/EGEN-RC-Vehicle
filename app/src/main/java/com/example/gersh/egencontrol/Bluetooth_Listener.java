ca2081f80dd2e948236f805744b7dd9470c30f73
Garret Gershmel <gershmail1@gmail.com>
2018-11-28 08:40:56 -0700

    Comments
    +Added and improved comments accross the source code

--- app/src/main/java/com/example/gersh/egencontrol/Bluetooth_Listener.java
+++ app/src/main/java/com/example/gersh/egencontrol/Bluetooth_Listener.java
@@ -1,3 +1,8 @@
+/*
+* Noah Gershmel
+* Class to receive bluetooth messages and report to the main activity
+*/
+
 package com.example.gersh.egencontrol;
 
 import android.bluetooth.BluetoothSocket;
@@ -7,47 +12,59 @@ import android.util.Log;
 import java.io.*;
 import java.net.*;
 
+//Extends thread to allow it to run indefinetely in the background
 public class Bluetooth_Listener extends Thread {
+    //The main thread and the socket it is using
     MainThread parent;
     BluetoothSocket importedSocket;
 
+    //A buffer to store the incoming message into
     private byte[] readBuffer;
     int readBufferPosition;
 
+    //Constructor requires a socket
     public Bluetooth_Listener(BluetoothSocket socket){
         importedSocket = socket;
     }
 
+    //Sets the parent to the main thread passed in
     public void setParent(MainThread c){
         parent = c;
     }
 
+    //Sets the socket to a new socket
     public void setImportedSocket(BluetoothSocket socket){
         importedSocket = socket;
     }
 
+    //Function that runs on a new thread
     public void run(){
+        //ASCII value for the delimiter character
         final byte delimiter = 10;
 
         try{
+            //Stores strings from the bluetooth module
             String fromClient;
             String toClient;
             InputStream m_stream = importedSocket.getInputStream();
 
             boolean run = true;
+            //Run indefinetely in the background
             while (run) {
                 int bytesAvailable = m_stream.available();
+                //As soon as bytes become available read them in
                 if (bytesAvailable >0) {
                     byte[] packetBytes = new byte[bytesAvailable];
-                    m_stream.read(packetBytes);
+                    m_stream.read(packetBytes); //Stores from the input to the stream
                     for (int i = 0; i < bytesAvailable; i++) {
+                        //Continue reading in bytes until delimiter is found
                         byte b = packetBytes[i];
                         if (b == delimiter){
                             byte[] encodedBytes = new byte[readBufferPosition];
                             System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
                             String data = new String(encodedBytes, "US-ASCII");
                             readBufferPosition = 0;
-                            handleMessage(data);
+                            handleMessage(data);    //Call the function to handle a new message
                         } else {
                             readBuffer[readBufferPosition++] = b;
                         }
@@ -60,6 +77,7 @@ public class Bluetooth_Listener extends Thread {
         }
     }
 
+    //Logs a new message so the user knows what information is being passed
     private void handleMessage(String message){
         Log.i("BT", "\n\n------------ " + message + " ------------\n\n");
     }
--- app/src/main/java/com/example/gersh/egencontrol/Bluetooth_Sender.java
+++ app/src/main/java/com/example/gersh/egencontrol/Bluetooth_Sender.java
@@ -1,31 +1,45 @@
+/*
+* Noah Gershmel
+* Runnable class to send commands over bluetooth
+*/
+
 package com.example.gersh.egencontrol;
 
 import android.bluetooth.BluetoothSocket;
 import android.util.Log;
 
+//Implements Runnable to allow for threading
 public class Bluetooth_Sender implements Runnable {
+    //Requires a socket and a message to send
     private BluetoothSocket btSocket;
     private String message;
 
+    //Constructor takes in the socket it will use for sending
     Bluetooth_Sender(BluetoothSocket new_btSocket){
         btSocket = new_btSocket;
     }
 
+    //Sets the message variable to prep for sending
     public void setMessage(String new_message){
         message = new_message;
     }
 
+    //Sets a new socket if the original had an error
     public void setBtSocket(BluetoothSocket new_btSocket){
         btSocket = new_btSocket;
     }
 
+    //Override the run function to spawn a thread in parrallel
     @Override
     public void run() {
+        //Log what is attempting to be sent
         Log.d("SendThread", "Sending: " + message);
         if (btSocket!=null){
             try {
+                //Translate the message to bytes and write to the sockets output stream
                 btSocket.getOutputStream().write(message.getBytes());
             } catch (Exception e){
+                //Log a failure in sending if one occurs
                 Log.d("BT", "Bluetooth Sending Failed");
                 e.printStackTrace();
             }
